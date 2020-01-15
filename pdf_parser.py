79c18c9c493d81c565bdbf0027d76b2362a3da25
Mauricio Araujo <mauricio.araujo@smartkode.mx>
2020-01-15 16:43:47 -0600

    Agregar índices

--- pdf_parser.py
+++ pdf_parser.py
@@ -6,9 +6,17 @@ import sys
 file_name = str(sys.argv[1])
 pattern =  str(sys.argv[2])
 pdf = PdfFileReader(open(file_name, "rb"))
+occurrences = 0
+indexes = []
 
 for pageNum in range(0, pdf.numPages):
     page = pdf.getPage(pageNum)
     text = page.extractText()
-    print(text.find(pattern))
-    #print(page.extractText())
+    count = text.count(pattern)
+
+    if count > 0:
+        occurrences += count
+        indexes.append(pageNum)
+
+print(occurrences, "ocurrencias del patrón en", pdf.numPages, "páginas")
+print(indexes)
